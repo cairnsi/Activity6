8e8a1246b116a10bfe40a49f35eef0c3e6f93cee
cairnsi <59540148+cairnsi@users.noreply.github.com>
2020-02-23 12:06:21 -0600

    added sessions to get

---
+++ Activity6Handlebars.js
@@ -0,0 +1,63 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({secret:'SuperSecretPassword'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3128);
+
+app.get('/',function(req,res){
+  var context= {};
+  context.name = "Not Set";
+  if(req.name){
+	  req.session.name = req.name
+	  
+  }
+  if(req.session.name){
+	  context.name = req.session.name;
+  }
+  var input = [];
+  for(var item in req.query){
+	  input.push({'name':item, 'value': req.query[item]})
+  }
+  context.inputData = input;
+  res.render('GET',context);
+});
+
+app.post('/', function(req,res){
+  var urlData=[];
+  for(var item in req.query){
+	  urlData.push({'name':item, 'value': req.query[item]})
+  }
+  var bodyData = [];
+  for (var item in req.body){
+	  bodyData.push({'name': item, 'value': req.body[item]})
+  }
+  var context = {};
+  context.urlInput = urlData;
+  context.bodyInput = bodyData;
+  res.render('POST', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
